db66b264241783cb83fa786bb22c340fd3391170
Bas Tichelaar <mail@bastichelaar.com>
2015-06-26 14:04:48 +0200

    Added ssh keys

--- consul.tf
+++ consul.tf
@@ -69,37 +69,6 @@ resource "aws_security_group" "allow_all" {
   }
 }
 
-#--------------------------------------------------------------
-# VPC
-#--------------------------------------------------------------
-#resource "aws_vpc" "main" {
-#    cidr_block = "172.31.0.0/16"
-#    enable_dns_hostnames = true
-#}
-#
-#resource "aws_subnet" "main" {
-#    vpc_id = "${aws_vpc.main.id}"
-#    cidr_block = "172.31.0.0/20"
-#    map_public_ip_on_launch = true
-#}
-#
-#resource "aws_internet_gateway" "gw" {
-#    vpc_id = "${aws_vpc.main.id}"
-#}
-#
-#resource "aws_route_table" "r" {
-#    vpc_id = "${aws_vpc.main.id}"
-#    route {
-#        cidr_block = "0.0.0.0/0"
-#        gateway_id = "${aws_internet_gateway.gw.id}"
-#    }
-#}
-#
-#resource "aws_main_route_table_association" "a" {
-#    vpc_id = "${aws_vpc.main.id}"
-#    route_table_id = "${aws_route_table.r.id}"
-#}
-
 resource "aws_key_pair" "consul" {
     key_name = "consul-key-pair"
     public_key = "${file(var.public_key)}"
