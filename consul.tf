f4b6e81416409d3ad54d67d5b4ee591277e700b0
Bas Tichelaar <mail@bastichelaar.com>
2015-06-26 14:08:58 +0200

    Added ssh keys

--- consul.tf
+++ consul.tf
@@ -63,8 +63,8 @@ resource "aws_security_group" "allow_all" {
 
   ingress {
       from_port = 0
-      to_port = 0
-      protocol = "-1"
+      to_port = 65535
+      protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
 }
